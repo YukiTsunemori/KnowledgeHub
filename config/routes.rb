bb9091d99a432a4aa6e41fb70d9819851c9da5fd
koikeasuka <ikeko@sonicgarden.jp>
2025-08-18 03:16:56 +0900

    クォート統一

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :admins, controllers: { sessions: 'admins/sessions', passwords: 'admins/passwords'}
+  devise_for :admins, controllers: { sessions: "admins/sessions", passwords: "admins/passwords"}
 
-  get "up" => "rails/health#show", as: :rails_health_check
   namespace :admins do
     root to: "home#index"
     resource :account, only: %i[edit update]
   end
+  get "up" => "rails/health#show", as: :rails_health_check
 end

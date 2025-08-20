3b6ba034b19a198e3bd66cd5f2d9e8a059f5ffb4
koikeasuka <ikeko@sonicgarden.jp>
2025-08-20 22:59:55 +0900

    新規作成後詳細画面へ遷移するように

--- app/controllers/admins/employees_controller.rb
+++ app/controllers/admins/employees_controller.rb
@@ -2,6 +2,7 @@ class Admins::EmployeesController < Admins::ApplicationController
   before_action :set_employee, only: %i[show]
 
   def show; end
+
   def new
     @employee = Employee.new
   end
@@ -10,13 +11,17 @@ class Admins::EmployeesController < Admins::ApplicationController
     @employee = Employee.new(employee_params)
 
     if @employee.save
-      redirect_to admins_employees_path(@employee), notice: "従業員アカウントを登録しました"
+      redirect_to admins_employee_path(@employee), notice: "従業員アカウントを登録しました"
     else
       render :new, status: :unprocessable_content
     end
   end
 
   private
+
+  def set_employee
+    @employee = Employee.find(params[:id])
+  end
 
   def employee_params
     params.expect(employee: %i[name email password password_confirmation])

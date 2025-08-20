8bd9c3fcf75ff11d9337bb19d3eadf20272e3701
koikeasuka <ikeko@sonicgarden.jp>
2025-08-20 22:34:39 +0900

    管理者が従業員アカウントを作成できるように

---
+++ app/controllers/admins/employees_controller.rb
@@ -0,0 +1,24 @@
+class Admins::EmployeesController < Admins::ApplicationController
+  before_action :set_employee, only: %i[show]
+
+  def show; end
+  def new
+    @employee = Employee.new
+  end
+
+  def create
+    @employee = Employee.new(employee_params)
+
+    if @employee.save
+      redirect_to admins_employees_path(@employee), notice: "従業員アカウントを登録しました"
+    else
+      render :new, status: :unprocessable_content
+    end
+  end
+
+  private
+
+  def employee_params
+    params.expect(employee: %i[name email password password_confirmation])
+  end
+end

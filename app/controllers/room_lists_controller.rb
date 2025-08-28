b9487680b82e083c3ec8c64b7591642136480fce
yuki.tsunemori <yuki.tsunemori1229@gmail.com>
2025-08-28 18:31:59 +0900

    room_lists_controllerの作成

---
+++ app/controllers/room_lists_controller.rb
@@ -0,0 +1,10 @@
+class RoomListsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get "room_lists/index"
+  get "room_lists/new"
+  get "room_lists/show"
   devise_for :admins, controllers: { sessions: "admins/sessions", passwords: "admins/passwords"}
 
   namespace :admins do

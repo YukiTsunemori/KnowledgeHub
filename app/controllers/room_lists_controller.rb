e0e31b2647f39075c6b66154e55f243aae9ece63
yuki.tsunemori <yuki.tsunemori1229@gmail.com>
2025-08-29 15:50:43 +0900

    ルーム情報の登録画面を実装しました。

--- app/controllers/room_lists_controller.rb
+++ app/controllers/room_lists_controller.rb
@@ -3,8 +3,27 @@ class RoomListsController < ApplicationController
   end
 
   def new
+    @categories = RoomCategory.all
+    @amenities = AmenityGroup.all
+    # binding.irb
+    @room = RoomList.new
+  end
+
+  def create
+    # bindig.irb
+    @room = RoomList.new(room_list_parameter)
+    if @room.save
+      redirect_to new_room_lists_path, notice: 'ルーム情報は無事作成されました。'
+    else
+      render :new, flash.now[:notice]
+    end
   end
 
   def show
   end
+
+  private
+    def room_list_parameter
+      params.require(:room_list).permit(:room_type_name, :square_meters, :capacity, :bed_type, :bed_quantity, :htwn, :room_category_id, :amenity_group_id)
+    end
 end

25d9e2a94d6aa1e5f08f4d942ec2ef90fbef7d1c
yuki.tsunemori <yuki.tsunemori1229@gmail.com>
2025-09-19 17:25:10 +0900

    不要なルーティングを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root "chats#index"
-  get "facilities/new"
-  get "facilities/show"
   get "configurations/index", to: "configurations#index"
   devise_for :employees
   devise_for :admins, controllers: { sessions: "admins/sessions", passwords: "admins/passwords" }
@@ -11,10 +9,10 @@ Rails.application.routes.draw do
     resource :account, only: %i[edit update]
     resources :employees
   end
-  resource :room_categories
-  resource :amenity_groups
-  resource :room_lists
-  resource :facilities
+  resources :room_categories
+  resources :amenity_groups
+  resources :room_lists
+  resources :facilities
 
   resources :chats, only: [ :index, :new, :create, :show ] do
   resources :messages, only: [ :create ]

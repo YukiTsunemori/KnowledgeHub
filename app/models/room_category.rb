2a1cfe95bd577eb6d2429f02e814b4071938527f
koikeasuka <ikeko@sonicgarden.jp>
2025-09-20 01:44:10 +0900

    room categoryリファクタ

--- app/models/room_category.rb
+++ app/models/room_category.rb
@@ -2,4 +2,6 @@ class RoomCategory < ApplicationRecord
   has_many :room_lists, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
+
+  scope :default_order, -> { order(id: :desc) }
 end

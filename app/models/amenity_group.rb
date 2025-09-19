0cc5ee0fe4a72a587327782bef0f74b37fc233f9
koikeasuka <ikeko@sonicgarden.jp>
2025-09-20 01:38:59 +0900

    リレーション追加

--- app/models/amenity_group.rb
+++ app/models/amenity_group.rb
@@ -1,4 +1,6 @@
 class AmenityGroup < ApplicationRecord
+  has_many :room_lists, dependent: :destroy
+
   validates :name, presence: true, uniqueness: true
   validates :content, presence: true
 
--- app/models/room_category.rb
+++ app/models/room_category.rb
@@ -1,3 +1,5 @@
 class RoomCategory < ApplicationRecord
+  has_many :room_lists, dependent: :destroy
+
   validates :name, presence: true, uniqueness: true
 end

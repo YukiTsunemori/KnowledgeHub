a3b001e2cef563b980b0be7e41084c0749cc81ba
yuki.tsunemori <yuki.tsunemori1229@gmail.com>
2025-09-27 15:24:53 +0900

    embedding_serviceをupdateとcreate用で分けました

---
+++ app/services/create_embedding_service.rb
@@ -0,0 +1,45 @@
+class CreateEmbeddingService
+  def self.create_for_roomlists(room)
+    content = ChunkGenerator.roomlists_chunk(room)
+    embedding = RubyLLM.embed(content, dimensions: 1536).vectors
+
+    KnowledgeChunk.create!(
+      content: content,
+      embedding: embedding
+    )
+    KnowledgeChunk.last.id
+  end
+
+  def self.create_for_roomcategory(category)
+    content = ChunkGenerator.roomcategories_chunk(category)
+    embedding = RubyLLM.embed(content, dimensions: 1536).vectors
+
+    KnowledgeChunk.create!(
+      content: content,
+      embedding: embedding
+    )
+    KnowledgeChunk.last.id
+  end
+
+  def self.create_for_amenitygroups(amenity)
+    content = ChunkGenerator.amenitygroups_chunk(amenity)
+    embedding = RubyLLM.embed(content, dimensions: 1536).vectors
+
+    KnowledgeChunk.create!(
+      content: content,
+      embedding: embedding
+    )
+    KnowledgeChunk.last.id
+  end
+
+  def self.create_for_facilities(facility)
+    content = ChunkGenerator.facilities_chunk(facility)
+    embedding = RubyLLM.embed(content, dimensions: 1536).vectors
+
+    KnowledgeChunk.create!(
+      content: content,
+      embedding: embedding
+    )
+    KnowledgeChunk.last.id
+  end
+end

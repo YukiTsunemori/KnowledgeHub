989d84e738f48b22b98b0a6028b5fff26e8545eb
koikeasuka <ikeko@sonicgarden.jp>
2025-09-29 22:07:57 +0900

    root変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :employees, controllers: { sessions: "employees/sessions", passwords: "employees/passwords" }
   devise_for :admins, controllers: { sessions: "admins/sessions", passwords: "admins/passwords" }
 
+  root to: redirect('/employees/sign_in')
+
   namespace :admins do
     root to: "home#index"
     resource :account, only: %i[edit update]

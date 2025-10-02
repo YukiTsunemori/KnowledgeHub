ece10b1d7d6486c55640dff28e1f855631f2b51d
yuki.tsunemori <yuki.tsunemori1229@gmail.com>
2025-10-02 14:13:33 +0900

    タイポ修正

--- spec/system/employee/configurations_spec.rb
+++ spec/system/employee/configurations_spec.rb
@@ -12,17 +12,17 @@ RSpec.describe 'configuraion', type: :system do
     end
 
     it "ルームカテゴリーボタンが存在し、リンクが正しいこと" do
-      expect(page).to have_link("ルームカテゴリー設定", href: employee_room_categories_path)
+      expect(page).to have_link("ルームカテゴリー設定", href: new_employees_room_category_path)
     end
 
     it "アメニティーグループボタンが存在し、リンクが正しいこと" do
       expect(page).to have_content("アメニティー")
-      expect(page).to have_link("アメニティー設定", href: employee_amenity_groups_path)
+      expect(page).to have_link("アメニティー設定", href: new_employees_amenity_group_path)
     end
 
     it "ルームリストボタンが存在し、リンクが正しいこと" do
       expect(page).to have_content("ルームリスト")
-      expect(page).to have_link("ルームリスト設定", href: employee_room_lists_path)
+      expect(page).to have_link("客室設定", href: new_employees_room_list_path)
     end
   end
 end

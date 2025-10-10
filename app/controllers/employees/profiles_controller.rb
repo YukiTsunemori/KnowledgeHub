60972f0d0c7bcf82b9b2869a659d0e7a8ffbd242
koikeasuka <ikeko@sonicgarden.jp>
2025-10-11 03:03:56 +0900

    プロフィールを登録できるように

--- app/controllers/employees/profiles_controller.rb
+++ app/controllers/employees/profiles_controller.rb
@@ -6,4 +6,20 @@ class Employees::ProfilesController < Employees::ApplicationController
   def new
     @profile = current_employee.profile || current_employee.build_profile
   end
+
+  def create
+    @profile = current_employee.build_profile(profile_params)
+
+    if @profile.save
+      redirect_to employees_profile_path, notice: "プロフィールを登録しました"
+    else
+      render :new, status: :unprocessable_content
+    end
+  end
+
+  private
+
+  def profile_params
+    params.expect(profile: %i[nickname birthday living_location hometown interest bio])
+  end
 end

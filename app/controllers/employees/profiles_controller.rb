70b9d847e390ca03721782320b7a46f19db1c72f
koikeasuka <ikeko@sonicgarden.jp>
2025-10-11 09:30:40 +0900

    プロフィール更新できるように

--- app/controllers/employees/profiles_controller.rb
+++ app/controllers/employees/profiles_controller.rb
@@ -1,4 +1,6 @@
 class Employees::ProfilesController < Employees::ApplicationController
+  before_action :set_profile, only: %i[edit update]
+
   def show
     @profile = current_employee.profile
   end
@@ -17,9 +19,24 @@ class Employees::ProfilesController < Employees::ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @profile.update(profile_params)
+      redirect_to employees_profile_path, notice: "プロフィールを更新しました"
+    else
+      render :edit, status: :unprocessable_content
+    end
+  end
+
   private
 
   def profile_params
     params.expect(profile: %i[nickname birthday living_location hometown interest bio])
+  end
+
+  def set_profile
+    @profile = current_employee.profile
   end
 end
